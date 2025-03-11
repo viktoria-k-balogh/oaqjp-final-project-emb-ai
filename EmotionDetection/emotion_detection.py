726aca1f7584a24c98f5d04696265a18d10a8a27
viktoria-k-balogh <vikina0929@gmail.com>
2025-03-11 11:54:46 -0400

    Saving

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,4 +6,13 @@ def emotion_detector(text_to_analyse):
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj={ "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+
+    formatted_response= json.loads(response.text)
+
+    emotions_dict=formatted_response['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion=str(max(emotions_dict, key=emotions_dict.get))
+    emotions_dict['dominant_emotion']=dominant_emotion
+
+    return emotions_dict
+        
---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detecto")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    return ("For the given statement, the system response is 'anger': {}, 'disgust':{}, 
+    'fear':{}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(response
+    ['anger'],response['disgust'],response['fear'],response['joy'],response['sadness',
+    response['dominant_emotion']]))
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
